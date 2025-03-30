22cea372a8304ae2a505f894a9fb7d36ec7cc052
Anukeerthana1910 <150768210+Anukeerthana1910@users.noreply.github.com>
2025-03-30 19:53:13 +0530

    delete playground-1.mongodb.js

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -6,7 +6,8 @@
 // The result of the last command run in a playground is shown on the results panel.
 // By default the first 20 documents will be returned with a cursor.
 // Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
+// For more documentation on playgrounds please refe
+r to
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
 // Select the database to use.
